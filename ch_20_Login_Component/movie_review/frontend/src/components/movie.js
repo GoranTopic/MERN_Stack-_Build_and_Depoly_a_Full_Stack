77049909de85c4304e6af8a441250723669c4779
telix <telix@telegonicaxx@live.com>
2021-09-06 14:01:52 -0700

    ended chapter 20

--- ch_20_Login_Component/movie_review/frontend/src/components/movie.js
+++ ch_20_Login_Component/movie_review/frontend/src/components/movie.js
@@ -11,6 +11,10 @@ import Row from  'react-bootstrap/Row';
 import Button from  'react-bootstrap/Button';
 import Media from 'react-bootstrap/Media';
 
+// import moment lib
+import moment from 'moment';
+
+
 const Movie = props => {
 		const [ movie, setMovie ] = useState({ id: null, title:"", rated:"", reviews: [] });
 
@@ -27,6 +31,41 @@ const Movie = props => {
 								console.log(response.data);
 						}).catch(e => {console.log(e)});
 		}
+
+	
+		const renderReview = (review, index) => {
+				return( 
+						<Media key={index}>
+								<Media.Body>
+										<h5> 
+												{review.name + "reviewed on "} 
+												{moment(review.date).format("Do MMMM YYYY")}
+										</h5>
+										<p> {review.review} </p>
+										{ props.user && props.user.id === review.user_id && 
+										<Row> 
+												<Col> 
+														<Link to={{ 
+																pathname:"/movies/" + props.match.params.id + "/review", 
+																state: {
+																		currentReview: review
+																} 
+														}}>
+																Edit
+														</Link>
+												</Col>
+												<Col>
+														<Button variant="link">
+																Delete
+														</Button>
+												</Col>
+										</Row> 
+										}
+								</Media.Body>
+						</Media>
+				);
+		}
+
 
 		return(
 				<div>
@@ -48,17 +87,14 @@ const Movie = props => {
 																</Link> }
 														</Card.Body>
 												</Card>
-												<br></br>
 												<h2>Reviews</h2>
+												<br></br>
+												{ movie.reviews.map(renderReview) }
 										</Col>
 								</Row>
 						</Container>
 				</div>
 		);
-		
-
-
-
 
 }
 
